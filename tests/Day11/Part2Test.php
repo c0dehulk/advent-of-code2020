db990fff3e3b588fb8cf7656e9581e76795083a5
Rob Piper <rpiper@workforcesoftware.com>
2020-12-15 18:41:29 +0000

    Added unit tests for day 11.

---
+++ tests/Day11/Part2Test.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+
+namespace Codehulk\AdventOfCode2020\Day11;
+
+use Codehulk\AdventOfCode2020\Framework\Output;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * (Describe test requirements for class)
+ */
+class Part2Test extends TestCase
+{
+    /**
+     * (Describe circumstances and expected behaviour of test case)
+     *
+     * @dataProvider dataPart1
+     */
+    public function testSomething(string $input, int $expected): void
+    {
+        $solution = new Part2();
+        $output = $solution->run($input, $this->createMock(Output::class));
+        self::assertSame($expected, $output);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @return mixed[]
+     */
+    public function dataPart1(): array
+    {
+        return [
+            'Example' => [
+                'input'    => <<<INPUT
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+INPUT,
+                'expected' => 26,
+            ],
+        ];
+    }
+}
